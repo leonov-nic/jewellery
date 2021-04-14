ef9735e769f3c86cf735f496767049e1f4dafa65
Leonov-nick <leonov-nic@yandex.ru>
2021-04-14 23:42:50 +0300

    14 апреля end

---
+++ build/js/vendor.js
@@ -0,0 +1,82 @@
+'use strict';
+
+(function () {
+
+  $(document).ready(function() {
+
+    var openItems = function () {
+      $(".faq__item").children('p').slideUp(200).removeClass('faq__item--opened');
+      $(".faq__item").removeClass('faq__item--opened');
+      $(".faq__item").addClass('faq__item--closed');
+
+      $(this).children('p').slideDown(200).removeClass('faq__item--closed');
+      $(this).removeClass('faq__item--closed');
+      $(this).addClass('faq__item--opened');
+    }
+
+    var openFiltertField = function () {
+      $("fieldset").children('.catalog__fields-box').slideUp(200).removeClass('catalog__js-opened');
+      $("fieldset").removeClass('catalog__js-opened');
+      $("fieldset").addClass('catalog__js-closed');
+
+      $(this).next('.catalog__fields-box').slideDown(200).removeClass('catalog__js-closed');
+      $(this).parents('fieldset').removeClass('catalog__js-closed');
+      $(this).parents('fieldset').addClass('catalog__js-opened');
+    }
+
+    $(".faq__item").on("click", openItems);
+    $("fieldset h3").on("click", openFiltertField);
+
+  });
+
+  var swiper = new Swiper('.swiper-container', {
+    spaceBetween: 30,
+    slidesPerView: 4,
+    slidesPerGroup: 4,
+    loopFillGroupWithBlank: true,
+    // centeredSlides: true,
+    breakpointsInverse: true,
+    breakpoints: {
+      320: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+        slidesPerView: 2,
+        slidesPerGroup: 2,
+
+        pagination: {
+          el: ".swiper-pagination",
+          type: 'custom',
+          renderCustom: function (swiper, current, total) {
+            return (current) + ' of ' + (total);
+          }
+        },
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+        slidesPerView: 2,
+        slidesPerGroup: 2
+      },
+      1024: {
+        slidesPerView: 4,
+        spaceBetween: 30
+      }
+    },
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      renderBullet: function (index, className) {
+        return '<span class="' + className + '">' + (index + 1) + '</span>';
+      },
+    },
+    navigation: {
+      nextEl: '.swiper-button-nex',
+      prevEl: '.swiper-button-pre',
+    },
+  });
+
+})();
