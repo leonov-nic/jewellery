3c70ee2d9f3b47d772b9ce89ee54dc7ddbfac0d4
Leonov-nick <leonov-nic@yandex.ru>
2021-04-19 20:20:09 +0300

    19 апреля первые правки

--- build/js/vendor.js
+++ build/js/vendor.js
@@ -1,6 +1,7 @@
 'use strict';
 
 (function () {
+  var swiperContainer = document.querySelector(".swiper-container");
 
   $(document).ready(function() {
 
@@ -29,54 +30,55 @@
 
   });
 
-  var swiper = new Swiper('.swiper-container', {
-    spaceBetween: 30,
-    slidesPerView: 4,
-    slidesPerGroup: 4,
-    loopFillGroupWithBlank: true,
-    // centeredSlides: true,
-    breakpointsInverse: true,
-    breakpoints: {
-      320: {
-        slidesPerView: 2,
-        spaceBetween: 30,
-        slidesPerView: 2,
-        slidesPerGroup: 2,
+  if (swiperContainer) {
 
-        pagination: {
-          el: ".swiper-pagination",
-          type: 'custom',
-          renderCustom: function (swiper, current, total) {
-            return (current) + ' of ' + (total);
-          }
+    var swiper = new Swiper('.swiper-container', {
+      spaceBetween: 30,
+      slidesPerView: 4,
+      slidesPerGroup: 4,
+      loopFillGroupWithBlank: true,
+      breakpointsInverse: true,
+      breakpoints: {
+        320: {
+          spaceBetween: 30,
+          slidesPerView: 2,
+          slidesPerGroup: 2,
+
+          pagination: {
+            el: ".swiper-pagination",
+            type: 'custom',
+            renderCustom: function (swiper, current, total) {
+              return (current) + ' of ' + (total);
+            }
+          },
+        },
+        768: {
+          spaceBetween: 30,
+          slidesPerView: 2,
+          slidesPerGroup: 2
+        },
+        1024: {
+          slidesPerView: 4,
+          spaceBetween: 30
+        }
+      },
+      autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        renderBullet: function (index, className) {
+          return '<span class="' + className + '">' + (index + 1) + '</span>';
         },
       },
-      768: {
-        slidesPerView: 2,
-        spaceBetween: 30,
-        slidesPerView: 2,
-        slidesPerGroup: 2
+      navigation: {
+        nextEl: '.swiper-button-nex',
+        prevEl: '.swiper-button-pre',
       },
-      1024: {
-        slidesPerView: 4,
-        spaceBetween: 30
-      }
-    },
-    autoplay: {
-      delay: 3000,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-      renderBullet: function (index, className) {
-        return '<span class="' + className + '">' + (index + 1) + '</span>';
-      },
-    },
-    navigation: {
-      nextEl: '.swiper-button-nex',
-      prevEl: '.swiper-button-pre',
-    },
-  });
+    });
+
+  }
 
 })();
